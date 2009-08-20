ab91e33242048d32f41279a7681fdf0cc9e0d64c
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-08-20 12:54:20 +0000

    fix compile problem

--- src/main/java/de/ingrid/admin/IndexController.java
+++ src/main/java/de/ingrid/admin/IndexController.java
@@ -2,8 +2,8 @@ package de.ingrid.admin;
 
 import java.lang.Thread.State;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,7 +16,7 @@ public class IndexController {
 
     private Thread _thread = null;
     private final IndexRunnable _indexRunnable;
-    private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
+    private static final Log LOG = LogFactory.getLog(IndexController.class);
 
     @Autowired
     public IndexController(IndexRunnable indexRunnable) {
