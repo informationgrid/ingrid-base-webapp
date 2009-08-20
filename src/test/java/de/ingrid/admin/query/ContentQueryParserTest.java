bfea2a701af1d8ffdbc05d94420f41d3995c1758
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-08-20 13:45:01 +0000

    fix tests

--- src/test/java/de/ingrid/admin/query/ContentQueryParserTest.java
+++ src/test/java/de/ingrid/admin/query/ContentQueryParserTest.java
@@ -1,33 +1,25 @@
 package de.ingrid.admin.query;
 
-import java.lang.reflect.Method;
+import junit.framework.TestCase;
 
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.BooleanClause.Occur;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 
 import de.ingrid.utils.query.IngridQuery;
 import de.ingrid.utils.queryparser.QueryStringParser;
 
-public class ContentQueryParserTest {
+public class ContentQueryParserTest extends TestCase {
 
-    @DataProvider(name = "termQuery")
-    public Object[][] createData(Method m) {
-        return new Object[][] { new Object[] { "foo" } };
-    }
-
-    @Test(dataProvider = "termQuery")
     public void testParseContent(String query) throws Exception {
-        IngridQuery ingridQuery = QueryStringParser.parse(query);
+        IngridQuery ingridQuery = QueryStringParser.parse("foo");
         BooleanQuery booleanQuery = new BooleanQuery();
         IQueryParser parser = new ContentQueryParser();
         parser.parse(ingridQuery, booleanQuery);
         assert booleanQuery.getClauses().length == 1;
         BooleanClause booleanClause = booleanQuery.getClauses()[0];
-        assert booleanClause.getOccur() == Occur.MUST;
-        assert booleanClause.getQuery().toString().equals("content:foo");
+        assertEquals(Occur.MUST, booleanClause.getOccur());
+        assertEquals(booleanClause.getQuery().toString(), "content:foo");
     }
 
 }
