f052a81b2d7ee55e8f91f999b726827bb0499d56
me.101tec <me.101tec@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-10-06 13:38:00 +0000

    improving api

---
+++ src/test/java/de/ingrid/admin/service/DummyProducer.java
@@ -0,0 +1,53 @@
+package de.ingrid.admin.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Index;
+import org.apache.lucene.document.Field.Store;
+
+import de.ingrid.admin.object.IDocumentProducer;
+
+public class DummyProducer implements IDocumentProducer {
+
+    private List<Document> _dummys;
+
+    private Iterator<Document> _iterator;
+
+    @Override
+    public boolean hasNext() {
+        return _iterator.hasNext();
+    }
+
+    @Override
+    public void initialize() throws Exception {
+        _dummys = new ArrayList<Document>();
+        _dummys.add(createDocument("Max", "Ender", "male", "08.12.1988"));
+        _dummys.add(createDocument("Marko", "Bauhardt", "male", "30.07.1978"));
+        _dummys.add(createDocument("Andreas", "KŸster", "male", "01.01.1970"));
+        _dummys.add(createDocument("Frank", "Henze", "male", "01.01.1970"));
+
+        _iterator = _dummys.iterator();
+    }
+
+    @Override
+    public Document next() {
+        return _iterator.next();
+    }
+
+    private Document createDocument(final String first, final String last, final String gender, final String birthday) {
+        final Document doc = new Document();
+        doc.add(createField("first", first));
+        doc.add(createField("last", last));
+        doc.add(createField("gender", gender));
+        doc.add(createField("birthdate", birthday));
+        return doc;
+    }
+
+    private Field createField(final String key, final String value) {
+        return new Field(key, value, Store.YES, Index.ANALYZED);
+    }
+}
---
+++ src/test/java/de/ingrid/admin/service/IndexRunnableTest.java
@@ -0,0 +1,67 @@
+package de.ingrid.admin.service;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+import org.apache.lucene.index.IndexReader;
+
+import de.ingrid.admin.TestUtils;
+import de.ingrid.utils.PlugDescription;
+
+public class IndexRunnableTest extends TestCase {
+
+    private IndexRunnable _indexRunnable;
+
+    private PlugDescription _plugDescription;
+
+    private File _file;
+
+    @Override
+    protected void setUp() {
+        _file = new File(System.getProperty("java.io.tmpdir"), this.getClass().getName());
+        assertTrue(_file.exists() ? true : _file.mkdirs());
+        _plugDescription = new PlugDescription();
+        _plugDescription.setWorkinDirectory(_file);
+
+        final LuceneSearcher luceneSearcher = new LuceneSearcher();
+        luceneSearcher.configure(_plugDescription);
+        final RestartSearcherListener restartSearcherListener = new RestartSearcherListener(luceneSearcher);
+        restartSearcherListener.configure(_plugDescription);
+        final FlipIndexListener flipIndexListener = new FlipIndexListener(restartSearcherListener);
+        flipIndexListener.configure(_plugDescription);
+        _indexRunnable = new IndexRunnable(flipIndexListener);
+        _indexRunnable.configure(_plugDescription);
+        _indexRunnable.setDocumentProducer(new DummyProducer());
+        _indexRunnable.run();
+    }
+
+    @Override
+    protected void tearDown() {
+        TestUtils.delete(_file);
+    }
+
+    public void testIndexExists() {
+        final File file = new File(_file, "index");
+        assertTrue(file.exists());
+        assertTrue(file.list().length > 0);
+    }
+
+    public void testReadIndex() throws Exception {
+        final IndexReader reader = IndexReader.open(new File(_file, "index"));
+
+        assertEquals(4, reader.maxDoc());
+
+        assertEquals("Max", reader.document(0).get("first"));
+        assertEquals("08.12.1988", reader.document(0).get("birthdate"));
+
+        assertEquals("Marko", reader.document(1).get("first"));
+        assertEquals("male", reader.document(1).get("gender"));
+
+        assertEquals("Andreas", reader.document(2).get("first"));
+        assertEquals("KŸster", reader.document(2).get("last"));
+
+        assertEquals("Frank", reader.document(3).get("first"));
+        assertNull(reader.document(3).get("nick"));
+    }
+}
