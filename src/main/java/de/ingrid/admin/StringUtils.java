044056ded1346841618d5d2476f775a5169a8d57
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-10-16 12:45:46 +0000

    add method to padd a number (used for indexing and searching)

--- src/main/java/de/ingrid/admin/StringUtils.java
+++ src/main/java/de/ingrid/admin/StringUtils.java
@@ -1,21 +1,37 @@
 package de.ingrid.admin;
 
+import java.text.DecimalFormat;
 
 public class StringUtils {
 
-    public static boolean isEmpty(final String s) {
-        if (null == s || s.length() <= 0) {
-            return true;
-        }
-        return false;
-    }
+	private static final DecimalFormat FORMAT = new DecimalFormat(
+			"0000000000.####");
 
-    public static boolean isEmptyOrWhiteSpace(final String s) {
-        if (!isEmpty(s)) {
-            if (s.trim().length() > 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+	public static boolean isEmpty(final String s) {
+		if (null == s || s.length() <= 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean isEmptyOrWhiteSpace(final String s) {
+		if (!isEmpty(s)) {
+			if (s.trim().length() > 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * This method fills a number with leading zeros. So we get a number with
+	 * ten digits before the point a and four digits past the point
+	 * 
+	 * @param number
+	 *            The number to pad.
+	 * @return The padded number as a string.
+	 */
+	public static String padding(double number) {
+		return FORMAT.format(number);
+	}
 }
