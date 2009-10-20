563ed6199e5fab36696b334178ee8a23d58d72a7
mb <mb@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-10-20 14:46:54 +0000

    fix getdertails

--- src/main/java/de/ingrid/admin/controller/AdminToolsController.java
+++ src/main/java/de/ingrid/admin/controller/AdminToolsController.java
@@ -24,81 +24,75 @@ import de.ingrid.utils.xml.PlugdescriptionSerializer;
 @Controller
 public class AdminToolsController extends AbstractController {
 
-	private final CommunicationService _communication;
+    private final CommunicationService _communication;
 
-	private final HeartBeatPlug _plug;
+    private final HeartBeatPlug _plug;
 
-	@Autowired
-	public AdminToolsController(final CommunicationService communication,
-			final HeartBeatPlug plug) throws Exception {
-		_communication = communication;
+    @Autowired
+    public AdminToolsController(final CommunicationService communication, final HeartBeatPlug plug) throws Exception {
+        _communication = communication;
 
-		_plug = plug;
-		final File file = new File(System.getProperty("plugDescription"));
-		if (file.exists()) {
-			final PlugdescriptionSerializer serializer = new PlugdescriptionSerializer();
-			_plug.configure(serializer.deSerialize(file));
-		}
-	}
+        _plug = plug;
+        final File file = new File(System.getProperty("plugDescription"));
+        if (file.exists()) {
+            final PlugdescriptionSerializer serializer = new PlugdescriptionSerializer();
+            _plug.configure(serializer.deSerialize(file));
+        }
+    }
 
-	@RequestMapping(value = IUris.COMM_SETUP, method = RequestMethod.GET)
-	public String getCommSetup(final ModelMap modelMap) {
-		modelMap.addAttribute("connected", _communication.isConnected());
-		return IViews.COMM_SETUP;
-	}
+    @RequestMapping(value = IUris.COMM_SETUP, method = RequestMethod.GET)
+    public String getCommSetup(final ModelMap modelMap) {
+        modelMap.addAttribute("connected", _communication.isConnected());
+        return IViews.COMM_SETUP;
+    }
 
-	@RequestMapping(value = IUris.COMM_SETUP, method = RequestMethod.POST)
+    @RequestMapping(value = IUris.COMM_SETUP, method = RequestMethod.POST)
     public String postCommSetup(@RequestParam("action") final String action) throws Exception {
-		if ("shutdown".equals(action)) {
-			_communication.shutdown();
-		} else if ("restart".equals(action)) {
-			_communication.restart();
-		} else if ("start".equals(action)) {
-			_communication.start();
-		}
+        if ("shutdown".equals(action)) {
+            _communication.shutdown();
+        } else if ("restart".equals(action)) {
+            _communication.restart();
+        } else if ("start".equals(action)) {
+            _communication.start();
+        }
         return redirect(IUris.COMM_SETUP);
-	}
+    }
 
-	@RequestMapping(value = IUris.HEARTBEAT_SETUP, method = RequestMethod.GET)
-	public String getHeartbeat(final ModelMap modelMap) {
-		modelMap.addAttribute("enabled", _plug.sendingHeartBeats());
-		return IViews.HEARTBEAT_SETUP;
-	}
+    @RequestMapping(value = IUris.HEARTBEAT_SETUP, method = RequestMethod.GET)
+    public String getHeartbeat(final ModelMap modelMap) {
+        modelMap.addAttribute("enabled", _plug.sendingHeartBeats());
+        return IViews.HEARTBEAT_SETUP;
+    }
 
-	@RequestMapping(value = IUris.HEARTBEAT_SETUP, method = RequestMethod.POST)
-	public String setHeartBeat(@RequestParam("action") final String action)
-			throws IOException {
-		if ("start".equals(action)) {
-			_plug.startHeartBeats();
-		} else if ("stop".equals(action)) {
-			_plug.stopHeartBeats();
-		} else if ("restart".equals(action)) {
-			_plug.stopHeartBeats();
-			_plug.startHeartBeats();
-		}
+    @RequestMapping(value = IUris.HEARTBEAT_SETUP, method = RequestMethod.POST)
+    public String setHeartBeat(@RequestParam("action") final String action) throws IOException {
+        if ("start".equals(action)) {
+            _plug.startHeartBeats();
+        } else if ("stop".equals(action)) {
+            _plug.stopHeartBeats();
+        } else if ("restart".equals(action)) {
+            _plug.stopHeartBeats();
+            _plug.startHeartBeats();
+        }
         return redirect(IUris.HEARTBEAT_SETUP);
-	}
+    }
 
-	@RequestMapping(value = IUris.SEARCH, method = RequestMethod.GET)
-	public String showView(
-			final ModelMap modelMap,
-			@RequestParam(value = "query", required = false) final String queryString)
-			throws Exception {
-		if (queryString != null) {
-			modelMap.addAttribute("query", queryString);
-			final IngridQuery query = QueryStringParser.parse(queryString);
+    @RequestMapping(value = IUris.SEARCH, method = RequestMethod.GET)
+    public String showView(final ModelMap modelMap, @RequestParam(value = "query", required = false) final String queryString) throws Exception {
+        if (queryString != null) {
+            modelMap.addAttribute("query", queryString);
+            final IngridQuery query = QueryStringParser.parse(queryString);
 
-			final IngridHits results = _plug.search(query, 0, 20);
-			modelMap.addAttribute("totalHitCount", results.length());
+            final IngridHits results = _plug.search(query, 0, 20);
+            modelMap.addAttribute("totalHitCount", results.length());
 
-			final IngridHit[] hits = results.getHits();
-			final IngridHitDetail[] details = _plug.getDetails(hits, query,
-					null);
+            final IngridHit[] hits = results.getHits();
+            final IngridHitDetail[] details = _plug.getDetails(hits, query, new String[] { "title", "summary" });
 
-			modelMap.addAttribute("hitCount", details.length);
-			modelMap.addAttribute("hits", details);
-		}
+            modelMap.addAttribute("hitCount", details.length);
+            modelMap.addAttribute("hits", details);
+        }
 
-		return IViews.SEARCH;
-	}
+        return IViews.SEARCH;
+    }
 }
