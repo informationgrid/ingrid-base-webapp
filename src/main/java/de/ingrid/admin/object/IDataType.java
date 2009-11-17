9e13bf867cf9c2ebc3bd720135671bc15cc8cb59
me.101tec <me.101tec@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-11-17 15:23:01 +0000

    - removing default and dsc-other datatype from base webapp + adding data type locations to base webapp + adding default to xml and excel

--- src/main/java/de/ingrid/admin/object/IDataType.java
+++ src/main/java/de/ingrid/admin/object/IDataType.java
@@ -3,6 +3,30 @@ package de.ingrid.admin.object;
 
 public interface IDataType {
 
+    public static final String DEFAULT = "default";
+
+    public static final String DSC_ECS = "dsc_ecs";
+
+    public static final String DSC_ECS_ADDRESS = "dsc_ecs_address";
+
+    public static final String DSC_CSW = "dsc_csw";
+
+    public static final String DSC_RESEARCH = "dsc_research";
+
+    public static final String DSC_OTHER = "dsc_other";
+
+    public static final String METADATA = "metadata";
+
+    public static final String FIS = "fis";
+
+    public static final String LAW = "law";
+
+    public static final String ADDRESS = "address";
+
+    public static final String EXCEL = "excel";
+
+    public static final String XML = "xml";
+
     String getName();
 
     boolean isHidden();
