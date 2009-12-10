8cd80c06d30580b0b25b8c10fe2fb280ddeede22
me.101tec <me.101tec@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2009-12-10 16:20:04 +0000

    fixing session expired exception bug

--- src/main/java/de/ingrid/admin/controller/WelcomeController.java
+++ src/main/java/de/ingrid/admin/controller/WelcomeController.java
@@ -2,12 +2,14 @@ package de.ingrid.admin.controller;
 
 import java.io.File;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import de.ingrid.admin.IKeys;
 import de.ingrid.admin.IUris;
 import de.ingrid.admin.IViews;
 import de.ingrid.admin.command.PlugdescriptionCommandObject;
@@ -19,15 +21,17 @@ public class WelcomeController {
     private static PlugdescriptionCommandObject _plugDescription;
 
     @RequestMapping(value = IUris.WELCOME, method = RequestMethod.GET)
-    public String welcome(final ModelMap modelMap) throws Exception {
-        loadPlugDescription(modelMap);
+    public String welcome(final HttpSession session) throws Exception {
+        loadPlugDescription(session);
         return IViews.WELCOME;
     }
 
-    public PlugdescriptionCommandObject loadPlugDescription(final ModelMap modelMap) throws Exception {
+    public PlugdescriptionCommandObject loadPlugDescription(final HttpSession session) throws Exception {
         if (_plugDescription == null) {
             _plugDescription = new PlugdescriptionCommandObject(new File(System.getProperty("plugDescription")));
-            modelMap.addAttribute("plugDescription", _plugDescription);
+        }
+        if (session.getAttribute(IKeys.PLUG_DESCRIPTION) == null) {
+            session.setAttribute(IKeys.PLUG_DESCRIPTION, _plugDescription);
         }
         return _plugDescription;
     }
