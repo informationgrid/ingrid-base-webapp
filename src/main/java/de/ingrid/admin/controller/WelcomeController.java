c4e71c9dd7c86cdfaa43b95a3a0604da7c5f2781
aw <aw@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2010-03-15 09:35:58 +0000

    - fixed bug that webapp wouldn't start anymore because of missing autowiring (after refactoring)

--- src/main/java/de/ingrid/admin/controller/WelcomeController.java
+++ src/main/java/de/ingrid/admin/controller/WelcomeController.java
@@ -2,6 +2,7 @@ package de.ingrid.admin.controller;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,7 +18,8 @@ import de.ingrid.admin.service.PlugDescriptionService;
 public class WelcomeController {
 
     private final PlugDescriptionService _service;
-
+    
+    @Autowired
     public WelcomeController(final PlugDescriptionService service) {
         _service = service;
     }
