82bdf2aeba619f1145d9c97dcc3384f63f0691e9
mm <mm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2013-04-15 15:42:17 +0000

    base-webapp: - make GermanStemmer standard and provide spring example in xml for user defined stemmer/parser, see https://dev.wemove.com/jira/browse/INGRID-2246

--- src/main/java/de/ingrid/admin/search/GermanStemmer.java
+++ src/main/java/de/ingrid/admin/search/GermanStemmer.java
@@ -2,7 +2,9 @@ package de.ingrid.admin.search;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.de.GermanAnalyzer;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GermanStemmer extends Stemmer {
 
     private static final Analyzer ANALYZER = new GermanAnalyzer();
--- src/main/java/de/ingrid/admin/search/StandardStemmer.java
+++ src/main/java/de/ingrid/admin/search/StandardStemmer.java
@@ -3,9 +3,8 @@ package de.ingrid.admin.search;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.util.Version;
-import org.springframework.stereotype.Service;
 
-@Service
+
 public class StandardStemmer extends Stemmer {
 
     private static final Analyzer ANALYZER = new StandardAnalyzer(Version.LUCENE_CURRENT);
