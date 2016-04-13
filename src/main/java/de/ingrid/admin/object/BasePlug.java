adf075dcffaab13692389011f1cee2c4a5b4c710
Andre Wallat <andre.wallat@wemove.com>
2016-04-13 12:32:06 +0200

    - use latest deps versions - adapted sources

--- src/main/java/de/ingrid/admin/object/BasePlug.java
+++ src/main/java/de/ingrid/admin/object/BasePlug.java
@@ -27,6 +27,8 @@ import org.springframework.stereotype.Service;
 
 import de.ingrid.iplug.HeartBeatPlug;
 import de.ingrid.iplug.PlugDescriptionFieldFilters;
+import de.ingrid.utils.IngridCall;
+import de.ingrid.utils.IngridDocument;
 import de.ingrid.utils.IngridHit;
 import de.ingrid.utils.IngridHitDetail;
 import de.ingrid.utils.IngridHits;
@@ -65,5 +67,10 @@ public class BasePlug extends HeartBeatPlug {
             details[i] = new IngridHitDetail(hits[i], "Test Titel #" + (i + 1), "Test Summary #" + (i + 1));
         }
         return details;
+    }
+
+    @Override
+    public IngridDocument call(IngridCall targetInfo) throws Exception {
+        throw new RuntimeException( "call-function not implemented in Base-iPlug" );
     }
 }
